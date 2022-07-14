a57c1d69b05fde3e2ffe9d778389cf61d6744572
tajriba <quvvatullayev@gmail.com>
2022-07-14 16:57:43 +0300

    get_users_data.py

--- get_users_data.py
+++ get_users_data.py
@@ -1,4 +1,4 @@
-import get_data
+import json
 
 def get_users_data(data:dict) -> list:
     """
@@ -12,3 +12,23 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
+    list1 = []
+    for i in data["results"]:
+        dict1 = {}
+        for e,r in (i).items():
+            if e == "name":
+                for w,q in r.items():
+                    if w == "first" or w == "last":
+                        dict1[w] = q
+            elif e == "phone":
+                dict1[e] = r
+            list1.append(dict1)
+
+    list2 = []
+    for i in list1:
+        if i not in list2:
+            list2.append(i)
+
+    return list2
+
+print(get_users_data(json.loads(open("randomuser_data.json").read())))
