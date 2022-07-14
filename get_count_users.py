50ec2aab57a10e6ea229e0ac8969ee1abf83c8cb
tajriba <quvvatullayev@gmail.com>
2022-07-14 16:20:23 +0300

    get_count_users.py

--- get_count_users.py
+++ get_count_users.py
@@ -1,4 +1,3 @@
-from webbrowser import get
 import get_data
 
 def get_count_users(data:dict) -> int:
@@ -10,4 +9,10 @@ def get_count_users(data:dict) -> int:
     Returns:
         int: number of users
     """
-    
+    sum = 0
+    for i in data["results"]:
+        for e,r in dict(i).items():
+            if e == "email":
+                sum += 1
+    return sum
+print(get_count_users(get_data.get_data("randomuser_data.json")))
